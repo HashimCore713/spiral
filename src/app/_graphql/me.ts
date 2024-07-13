9db43fa35aad2e2e1cc7233f7261ce09cbfbbdc3
Hashims17 <hashims1713@gmail.com>
2024-07-14 01:51:38 +0500

    m

---
+++ src/app/_graphql/me.ts
@@ -0,0 +1,16 @@
+import { CART } from './cart'
+
+export const ME_QUERY = `query {
+  meUser {
+    user {
+      id
+      email
+      name
+      ${CART}
+      roles
+      address
+      phone
+    }
+    exp
+  }
+}`
