6190d78f259088023115a51df639a06a7caaee30
Hashims17 <hashims1713@gmail.com>
2024-07-14 17:12:42 +0500

    acc

---
+++ src/payload/globals/Settings.ts
@@ -0,0 +1,28 @@
+import type { GlobalConfig } from 'payload/types'
+
+export const Settings: GlobalConfig = {
+  slug: 'settings',
+  typescript: {
+    interface: 'Settings',
+  },
+  graphQL: {
+    name: 'Settings',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'productsPage',
+      type: 'relationship',
+      relationTo: 'pages',
+      label: 'Products page',
+    },
+    {
+      name: 'delivery',
+      label: 'Delivery Fee',
+      type: 'number',
+      required: true,
+    },
+  ],
+}
